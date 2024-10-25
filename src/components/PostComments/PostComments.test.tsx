bf94c22803f538736b17f970c9c7904fecc8d573
Erico Moraes <erico.moraes@bunzlbrasil.protcapad.com.br>
2024-10-25 18:07:59 -0300

    teste de inserção de comentario

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,25 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Deve adicionar "Comentário 1" na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Comentário 1'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Comentário 1')).toBeInTheDocument()
+    })
+
+    test('Deve adicionar "Comentário 2" na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Comentário 2'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('Comentário 2')).toBeInTheDocument()
+    })
 });
